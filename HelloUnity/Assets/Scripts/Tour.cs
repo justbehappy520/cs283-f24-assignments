00a022cd887215534ccb755adcb7616eb7d7ba93
Glory T Zhang <gzhang@brynmawr.edu>
2024-09-27 18:48:02 -0400

    Finished A04

--- HelloUnity/Assets/Scripts/Tour.cs
+++ HelloUnity/Assets/Scripts/Tour.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Tour : MonoBehaviour
 {
+    public Transform[] pointsOfInterest; // array of points of interest
+    public float speed = 2.0f; // speed of cam
+    private int currentIndex = 0; // current point of interst
+    private bool isMoving = false; // check if cam is moving
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,32 @@ public class Tour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       // check for key press
+       if (Input.GetKey(KeyCode.N) && !isMoving)
+        {
+            currentIndex = (currentIndex + 1) % pointsOfInterest.Length;
+            isMoving = true; // start moving
+        }
+
+       if (isMoving)
+        {
+            MoveToPoint(pointsOfInterest[currentIndex]);
+        }
+    }
+
+    // function to move cam between points of interest
+    private void MoveToPoint(Transform target)
+    {
+        transform.position = Vector3.Lerp(transform.position,
+            target.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+            target.rotation, speed * Time.deltaTime);
+
+        // check if the camera is near a point of interest
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            currentIndex = (currentIndex + 1) % pointsOfInterest.Length;
+            isMoving = !isMoving;
+        }
     }
 }
