bae5b57c4a290735b84c4e0946e6222557ffc38c
Glory T Zhang <gzhang@brynmawr.edu>
2024-10-25 14:27:39 -0400

    Finished A04

--- HelloUnity/Assets/Scripts/FlyCamera.cs
+++ HelloUnity/Assets/Scripts/FlyCamera.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class FlyCamera : MonoBehaviour
 {
-    public float speed = 50.0f; //max speed of camera
-    public bool smooth = true; // make camera movement smooth
-    public float acceleration = 0.05f;
-    private float actSpeed = 0.0f; // keep it between 0 and 1
-    private Vector3 lastDir = new Vector3(); // tracks the last dir of the cam
-    // track the last position of the mouse
-    private Vector3 lastMouse = new Vector3(255, 255, 255);
+    // rotation variables
+    public float sensX; // sensitivity along the x-axis
+    public float sensY; // sensitivity along the y-axis
+    float xRotation;
+    float yRotation;
+
+    // movement variables
+    public float moveSpeed;
+    float horiInput; // horizontal keyboard input
+    float vertInput; // vertical keyboard input
+    Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
@@ -22,38 +25,22 @@ public class FlyCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // rotation of the camera
-        lastMouse = Input.mousePosition - lastMouse;
-        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.y,
-            transform.eulerAngles.y + lastMouse.x, 0); // no rotate on z
-        transform.eulerAngles = lastMouse;
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        lastMouse = Input.mousePosition;
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // limit rotation to 90
 
-        // movement of the camera
-        Vector3 dir = new Vector3(); // create (0,0,0)
-        
-        if (Input.GetKey(KeyCode.W)) dir.z += 1.0f;
-        if (Input.GetKey(KeyCode.S)) dir.z -= 1.0f;
-        if (Input.GetKey(KeyCode.A)) dir.x -= 1.0f;
-        if (Input.GetKey(KeyCode.D)) dir.x += 1.0f;
-        dir.Normalize();
+        // rotate camera
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
 
-        if (dir != Vector3.zero)
-        {
-            // the camera is moving
-            if (actSpeed < 1) actSpeed += acceleration * Time.deltaTime;
-            else actSpeed = 1.0f;
+        // move camera
+        horiInput = Input.GetAxis("Horizontal");
+        vertInput = Input.GetAxis("Vertical");
+        dir = transform.forward * vertInput + transform.right * horiInput;
 
-            lastDir = dir;
-        } else
-        {
-            // the camera is not moving
-            if (actSpeed > 0) actSpeed -= acceleration * Time.deltaTime;
-            else actSpeed = 0.0f;
-        }
-
-        if (smooth) transform.Translate(lastDir * speed * Time.deltaTime);
-        else transform.Translate(dir * speed * Time.deltaTime);
+        transform.position += dir * moveSpeed;
     }
 }
--- HelloUnity/Assets/Scripts/Tour.cs
+++ HelloUnity/Assets/Scripts/Tour.cs
@@ -5,45 +5,59 @@ using UnityEngine;
 public class Tour : MonoBehaviour
 {
     public Transform[] pointsOfInterest; // array of points of interest
-    public float speed = 2.0f; // speed of cam
-    private int currentIndex = 0; // current point of interst
-    private bool isMoving = false; // check if cam is moving
+    //public Transform target;
+    public float moveSpeed = 2.0f; // movement speed of camera
+    public float rotateSpeed = 5.0f; // rotate speed of camera
+    private int index; // current point of interst
+    private bool isMoving; // check if cam is moving and keep cam moving
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        index = 0;
+        isMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       // check for key press
-       if (Input.GetKey(KeyCode.N) && !isMoving)
+        // check for key press
+        if (Input.GetKey(KeyCode.N))
         {
-            currentIndex = (currentIndex + 1) % pointsOfInterest.Length;
-            isMoving = true; // start moving
+            isMoving = true;
         }
-
-       if (isMoving)
+        if (isMoving)
         {
-            MoveToPoint(pointsOfInterest[currentIndex]);
+            MoveToPoint(pointsOfInterest[index]);
+            if (index == pointsOfInterest.Length)
+            {
+                index = 0;
+            }
         }
     }
 
     // function to move cam between points of interest
     private void MoveToPoint(Transform target)
     {
-        transform.position = Vector3.Lerp(transform.position,
-            target.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Slerp(transform.rotation,
-            target.rotation, speed * Time.deltaTime);
+        // move the camera
+        Vector3 transformPos = transform.position;
+        Vector3 targetPos = target.position;
+        transform.position = Vector3.Lerp(transformPos, targetPos, 
+            moveSpeed * Time.deltaTime);
+
+        // rotate the camera
+        Quaternion transformRot = transform.rotation;
+        Quaternion targetRot = target.rotation;
+        transform.rotation = Quaternion.Slerp(transformRot, targetRot,
+            rotateSpeed * Time.deltaTime);
 
         // check if the camera is near a point of interest
-        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f)
         {
-            currentIndex = (currentIndex + 1) % pointsOfInterest.Length;
-            isMoving = !isMoving;
+            transform.position = targetPos;
+            transform.rotation = targetRot;
+            isMoving = false;
+            index++;
         }
     }
 }
